f1b9391602007fe05186f86287243cde2b91d981
a.grachev <a.grachev@rwdt.ru>
2024-11-15 12:01:06 +0300

    first commit

---
+++ server.ts
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+io.on("connect_error", (err) => {
+  // the reason of the error, for example "xhr poll error"
+  console.log(err.message);
+
+  // some additional description, for example the status code of the initial HTTP response
+  console.log(err.description);
+
+  // some additional context, for example the XMLHttpRequest object
+  console.log(err.context);
+});
+
+server.listen(8090, () => {
+  console.log("listening on *:8090");
+});
